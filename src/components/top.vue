80eb6c2b0d62bb8f0d247c25d6dc37783551d912
Huaiku <39359219+Huaiku@users.noreply.github.com>
2018-12-23 17:47:13 +0800

    这是第一版

---
+++ src/components/top.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="top">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .top{
+        background: deeppink;
+        width: 100%;
+        height: 300px;
+    }
+</style>
